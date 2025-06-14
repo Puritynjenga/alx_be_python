902120e3b283bc187466319a78ded87b61945725
Purity Njenga <puritynjenga895@gmail.com>
2025-06-14 17:11:35 +0300

    modified bank account file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,9 +15,7 @@ class BankAccount:
         if self.account_balance >= amount:
             self.account_balance-+ amount
             return True
-        elif amount> self.account_balance:
-            print("Insufficient funds")
         else:
             return False
     def display_balance(self):
-            print(f"Current Balance: {self.account_balance: .2f}")
+            print(f"Current Balance: ${self.account_balance: .2f}")
