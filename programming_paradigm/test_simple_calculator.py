7431b4373adaad1d9d1022d2c52fb8fc2417246b
Purity Njenga <puritynjenga895@gmail.com>
2025-06-15 12:49:10 +0300

    modified testing files for a simple calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -10,15 +10,15 @@ class TestSimpleCalculator (unittest.TestCase):
         #test the addition method
         self.assertEqual(self.calc.add(2,3), 5)    
         self.assertEqual(self.calc.add(-1,1), 0)
-
+    def test_subtraction(self):
         #test the subtraction method
         self.assertEqual(self.calc.subtract(2,3), -1)
         self.assertEqual(self.calc.subtract(3,2), 1)
-
+    def test_multiplication(self):
         #test the multiply method
         self.assertEqual(self.calc.multiply(2,3), 5)
         self.assertEqual(self.calc.multiply(3,0), 0)
-
+    def test_division(self):
         #test the divide method
         self.assertEqual(self.calc.divide(6,3), 2)
         if self.assertEqual(self.calc.divide(6,0)):
