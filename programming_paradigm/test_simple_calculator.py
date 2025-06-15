65732a00b288e8d9911d585a5e11a5ab264c8c3c
Purity Njenga <puritynjenga895@gmail.com>
2025-06-15 12:46:07 +0300

    modified testing files for a simple calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -11,15 +11,16 @@ class TestSimpleCalculator (unittest.TestCase):
         self.assertEqual(self.calc.add(2,3), 5)    
         self.assertEqual(self.calc.add(-1,1), 0)
 
-        #test the subtract method
+        #test the subtraction method
         self.assertEqual(self.calc.subtract(2,3), -1)
         self.assertEqual(self.calc.subtract(3,2), 1)
 
         #test the multiply method
         self.assertEqual(self.calc.multiply(2,3), 5)
-        self.asserEqual(self.calc.multiply(3,0), 0)
+        self.assertEqual(self.calc.multiply(3,0), 0)
 
         #test the divide method
         self.assertEqual(self.calc.divide(6,3), 2)
         if self.assertEqual(self.calc.divide(6,0)):
             return ZeroDivisionError
+        
