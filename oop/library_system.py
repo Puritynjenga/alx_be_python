54ef705b07b913f44662ee8cd6f08e3c9e82a1a4
Purity Njenga <puritynjenga895@gmail.com>
2025-06-21 16:34:35 +0300

    modified library_system

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,16 +2,21 @@ class Book:
     def __init__(self, title:str, author:str):
         self.title = title
         self.author = author
-
+    def __str__(self):
+        return f"{self.title} by {self.author}"
 class EBook(Book):
     def __init__(self, title:str, author:str,file_format:int):
         super().__init__(title, author)
         self.file_format = file_format
+    def __str___(self):
+        return f"{self.title} by {self.author}, File Size: {self.file_format}KB"    
 class PrintBook(Book):
     def __init__(self, title:str, author:str, page_count:int):
         super().__init__(title,author)
         self.page_count = page_count
 
+    def __str__(self):
+        return f"{self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
     def __init__(self):
