ea26cb33e63c4ea11584e931fcaec165c7deea74
Puritynjenga <puritynjenga895@gmail.com>
2025-06-21 12:33:38 +0300

    added new files for book class for advanced oop

---
+++ oop/book_class.py
@@ -0,0 +1,22 @@
+
+#create a class called book that has the following attributes
+class Book:
+    def __init__(self, title:str ,author:str,year:int): #condtructor to initialize the attributes
+        self.title = title
+        self.author = author
+        self.year = year
+
+
+    def __str__(self):  #represent the book object in a readable string format
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def  __del__(self):
+        print(f"Deleting {self.title}")  #close the title of the book once the object is deleted
+
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+        
+         
+
+
+
